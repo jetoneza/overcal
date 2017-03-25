71720539397d2b95a348ac4793dc648a3ea60c11
Jethro Ordaneza <jet.oneza@gmail.com>
2017-03-25 13:05:02 +0800

    Setup bootstrap

--- webapp/src/routes/Home/components/HomeView.js
+++ webapp/src/routes/Home/components/HomeView.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
 import classes from './HomeView.scss'
+
+import { Button } from 'react-bootstrap'
 
 export const HomeView = () => (
   <div>
     <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className={classes.duck}
-      src={DuckImage} />
+    <Button bsStyle="primary">Hey</Button>
   </div>
 )
 
